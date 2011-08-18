9a58b8591e016810b57becb939426dacbdb193df
Patrik Weibull <patrik.weibull@gmail.com>
2011-08-18 06:50:04 +0200

    Main now takes startLevel as commandline parameter. Soundplater fixed.

--- src/chipschallenge/Main.java
+++ src/chipschallenge/Main.java
@@ -13,12 +13,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int startLevel = 1;
+        if(args.length > 0) {
+            try {
+                startLevel = Integer.parseInt(args[0]);
+            } catch(NumberFormatException ex) {
+                startLevel = 1;
+            }
+        }
         Game g = Game.getInstance();
         g.setLevelFactory(MicrosoftLevelFactory.getInstance());
         g.setBlockFactory(MicrosoftBlockFactory.getInstance());
         GUI Gui = GUI.getInstance();
         Gui.addKeyListener(ChipTickBehavior.getInstance());
-        g.nextLevel(76);
+        g.nextLevel(startLevel);
     }
 
 }
